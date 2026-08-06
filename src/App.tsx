f1ccab4302093b51dbc50af534c1f1b110ee46ff
thainguyethoa221286-hub <thainguyethoa221286@gmail.com>
2026-08-06 18:55:58 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Navbar } from './components/layout/Navbar';
 import { Sidebar, ActiveModule } from './components/layout/Sidebar';
 import StoreModule from './components/modules/StoreModule';
 import PRPOModule from './components/modules/PRPOModule';
-import { Module03LossDamage } from './components/modules/Module03LossDamage';
+import LossDamageModule from './components/modules/LossDamageModule';
 import { Module04Minibar } from './components/modules/Module04Minibar';
 import { Module05OfficeSupplies } from './components/modules/Module05OfficeSupplies';
 import { Module06Dashboard } from './components/modules/Module06Dashboard';
@@ -37,7 +37,7 @@ export default function App() {
               
              {activeModule === 'MODULE_01_STORE' && <StoreModule />}
               {activeModule === 'MODULE_02_PRPO' && <PRPOModule />}
-              {activeModule === 'MODULE_03_DAMAGE' && <Module03LossDamage />}
+              {activeModule === 'MODULE_03_DAMAGE' && <LossDamageModule />}
               {activeModule === 'MODULE_04_MINIBAR' && <Module04Minibar />}
               {activeModule === 'MODULE_05_VPP' && <Module05OfficeSupplies />}
               {activeModule === 'MODULE_06_DASHBOARD' && <Module06Dashboard />}
